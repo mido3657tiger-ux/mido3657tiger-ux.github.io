27d01b7c201a7d724129d1cc687a6af831554100
🔥 NovaDev <277220248+mido3657tiger-ux@users.noreply.github.com>
2026-04-26 20:12:56 +0300

    Add authentication check and logout functions

---
+++ auth.js
@@ -0,0 +1,12 @@
+function checkAuth(){
+  const token = localStorage.getItem("token");
+
+  if(!token){
+    window.location.href = "index.html";
+  }
+}
+
+function logout(){
+  localStorage.removeItem("token");
+  window.location.href = "index.html";
+}
