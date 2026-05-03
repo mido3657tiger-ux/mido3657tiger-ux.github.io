857e563d4f4e23e10dd3d994d8066d2949f30750
🔥 NovaDev <277220248+mido3657tiger-ux@users.noreply.github.com>
2026-05-03 19:26:04 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,20 +1,12 @@
-const express = require('express');
-const jwt = require('jsonwebtoken');
-const path = require('path');
+const express = require("express");
 const app = express();
 
-const CONFIG = { PORT: 3000, KEY: 'TIGER_CORE_XP' };
+app.use(express.static(__dirname));
 
-app.use(express.json());
-app.use(express.static(path.join(__dirname, '../frontend')));
-
-app.post('/api/auth', (req, res) => {
-    const { u, p } = req.body;
-    if (u === 'admin' && p === 'admin123') {
-        const token = jwt.sign({ access: 'root' }, CONFIG.KEY);
-        return res.json({ status: 200, token });
-    }
-    res.json({ status: 403 });
+app.get("/", (req,res)=>{
+res.sendFile(__dirname + "/index.html");
 });
 
-app.listen(CONFIG.PORT, () => console.log(`[SYSTEM] CORE ACTIVE: ${CONFIG.PORT}`));
+app.listen(3000, ()=>{
+console.log("Server running on http://localhost:3000");
+});
